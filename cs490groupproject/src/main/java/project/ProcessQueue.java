5db0bf9437a28ba988430b0e17e75e50bbc2bc25
Jake Maier <30730691+Maier-Jake@users.noreply.github.com>
2021-09-25 12:35:47 -0500

    Squashed commit of the following:
    commit 6738e5b77f83f0b3f8ee2b391610d8847ebdc3ca
    Author: Your Name <annaleisebk@outlook.com>
    Date:   Fri Sep 24 10:59:56 2021 -0500
    
        added class skeletons
    
    commit bbb419c33c7c4c1ddd71a9b72207b0e326efd228
    Author: SpiralGalaxy87 <annaleisebk@outlook.com>
    Date:   Thu Sep 23 11:15:44 2021 -0500
    
        Update README.md
    
    commit 4a0055c26d5c76b750d13a8aadc63f800bc29c1c
    Author: SpiralGalaxy87 <annaleisebk@outlook.com>
    Date:   Thu Sep 23 11:11:15 2021 -0500
    
        Update README.md
    
    commit c93a1f877f1b39768a0096f75fd79095a41aed30
    Author: SpiralGalaxy87 <annaleisebk@outlook.com>
    Date:   Thu Sep 23 11:10:39 2021 -0500
    
        Update README.md

---
+++ cs490groupproject/src/main/java/project/ProcessQueue.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Annaleise
+ */
+public class ProcessQueue {
+    private ArrayList<Process> processes;
+
+    public ProcessQueue() {
+        processes = new ArrayList<Process>();
+    }
+    
+    public void push(Process p){
+        
+    }
+    
+    public Process pop(){
+        return null;
+    }
+    
+}
