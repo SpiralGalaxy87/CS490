f82f3a8db5ea3dbb29acb54f044e71b979d4df30
jmp0060 <jmp0060@uah.edu>
2021-09-28 12:16:45 -0500

    moved main function from GUI_Driver to Main

--- cs490groupproject/src/main/java/project/Main.java
+++ cs490groupproject/src/main/java/project/Main.java
@@ -13,16 +13,45 @@ package project;
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import static project.GUI_Driver.os;
 
 public class Main {
     
-    public OS os;
+    public static OS os;
     public GUI_Driver gui;
     
     public static void main(String args[]) throws FileNotFoundException
-    {
-    
+    {    
         
+         /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(GUI_Driver.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(GUI_Driver.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(GUI_Driver.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GUI_Driver.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GUI_Driver(os).setVisible(true);
+            }
+        });
         /* Read in file to create processes and store in the ProcessQueue */
         // pass the path to the file as a parameter, create scanner
         File inFile = new File("input.txt");
@@ -47,14 +76,11 @@ public class Main {
             //priority
             int priority = lineSc.nextInt();
             
-            
             //Process newProcess = new Process(arrivalTime, processID, serviceTime, priority);
             
             //System.out.println(newProcess.display());
             
         }
-        //
-    
     }
     
     public Main()
