f8230c44dd58e2395bfa18b4fe856c00f117cb52
Your Name <annaleisebk@outlook.com>
2021-09-30 22:34:40 -0500

    store time remaining and pick back up mid-process on unpause

--- cs490groupproject/src/main/java/project/CPU.java
+++ cs490groupproject/src/main/java/project/CPU.java
@@ -26,7 +26,7 @@ public class CPU implements Runnable{
     public void displayStatus(){
         
     }
-    
+
     public void run(){
         while(this.readyQueue.size() > 0){
 
@@ -35,7 +35,7 @@ public class CPU implements Runnable{
                 this.timeRemaining = this.curProcess.getServiceTime();
             }   
            
-            System.out.println("  ...  cpu thread starting " + this.curProcess.getProcessID() + ", working for " + this.curProcess.getServiceTime() + " time units.");
+            System.out.println("  ...  cpu thread starting " + this.curProcess.getProcessID() + ", working for " + this.timeRemaining + " time units.");
 
             while(this.timeRemaining > 0){
                 try {
@@ -44,14 +44,11 @@ public class CPU implements Runnable{
                 } 
                 catch (InterruptedException ex) {
                 // TBD catch and deal with exception ere
-                    System.out.println("Exception caught: " + ex);
+                    System.out.println("Exception caught: " + ex + " with " + this.timeRemaining + " time remaining");
                     return;
                 }
             }
             System.out.println(this.curProcess.getProcessID() + " finished");
-
-            this.curProcess = this.readyQueue.dequeue();
-            this.timeRemaining = this.curProcess.getServiceTime();
         }
     }
 }
